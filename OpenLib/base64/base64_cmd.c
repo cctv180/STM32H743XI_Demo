292714313f87cde74dabc5a6cdf43f1eed75cbfa
180 cctv <cctv180@gmail.com>
2023-09-13 20:20:40 +0800

    优化base64_cmd chatGPT第1次优化
    1.使用函数显示错误消息：
    您在不同地方重复了错误消息打印代码。最好创建一个函数来显示错误消息，以避免代码重复。
    
    2.使用argc检查参数数量：
    与其在每个条件中检查argc，不如在函数开始处检查一次，然后根据需要处理错误。
    
    3.避免重复的代码：
    错误消息打印中有很多重复。您可以通过创建一个函数来打印用法说明和错误消息来减少这种重复。
    
    4.使用早期返回：
    不要深度嵌套if-else语句，而是使用早期返回来尽早退出函数，一旦遇到错误就返回。这简化了代码的结构。
    
    此优化后的代码更加模块化和易读，减少了冗余的代码，并提供了更清晰的错误消息和用法说明。

--- OpenLib/base64/base64_cmd.c
+++ OpenLib/base64/base64_cmd.c
@@ -9,200 +9,223 @@
 #include "dump_hex.h"
 #include "base64.h"
 
-#if defined(__SHELL_H__) && defined(DEBUG_MODE)
-static int _cmd(int argc, char *argv[])
+// 显示用法说明
+static void printUsage(const char *programName, const char *cmd)
 {
-#define __is_print(ch) ((unsigned int)((ch) - ' ') < 127u - ' ')
-#define HEXDUMP_WIDTH 16
+    printf("Error: Invalid command.\r\nUsage:\r\n");
+    printf("%s %s\r\n", programName, cmd);
+    printf("\r\n");
+}
 
-    const char *help_info[] = {"encode hex/str",
-                               "decode String"}; /* encode 编码 decode 解码 */
+// 显示错误消息
+static void printError(const char *message)
+{
+    printf("%s\r\n", message);
+}
 
-    // printf("\r\nargc = %d\r\n\r\n", argc);
-
-    if (!strcmp(argv[1], "encode")) // 编码
+// 输入编码为HEX
+static int encodeHex(int argc, char *argv[])
+{
+    if (argc <= 3)
     {
-        if (!strcmp(argv[2], "hex"))
+        printError("Parameter error.");
+        printUsage(argv[0], "encode hex \"0x** 0x** ...\"");
+        return -1;
+    }
+
+    uint16_t size = strlen(argv[3]);
+    uint8_t spaceCount = 0;
+
+    /* 计算空格数 */
+    for (size_t i = 0; i < size; i++)
+    {
+        if (argv[3][i] == ' ')
         {
-            if (argc <= 3)
-            {
-
-                printf("write parameter Error.\r\n");
-                printf("%s ", argv[0]);
-                printf("%s ", argv[1]);
-                printf("%s ", argv[2]);
-                printf("\"0x** 0x** ...\"\r\n");
-
-                return -1;
-            }
-
-            uint16_t size = strlen(argv[3]);
-            uint8_t spaceCount = 0;
-
-            /* 计算空格数 */
-            for (size_t i = 0; i < size; i++)
-            {
-                if (argv[3][i] == ' ')
-                {
-                    spaceCount++;
-                }
-            }
-            spaceCount += 2; // 至少2个数用于申请内存
-            // printf("spaceCount = %d\r\n", spaceCount);
-
-            /* 解码hex字符串 */
-            char *buff_hex = malloc(spaceCount); // 申请内存
-            if (!buff_hex)
-            {
-                printf("buff_hex Low memory! size = %d\r\n", spaceCount);
-
-                return -1;
-            }
-            char *p_end = argv[3];
-            char *p_old = 0;
-            size = 0; // 初始化hex长度
-            for (size_t i = 0; i < spaceCount; i++)
-            {
-                // 使用strtol函数将字符串转换为整数，并存储到buff_hex中
-                buff_hex[i] = strtol(p_end, &p_end, 0);
-
-                if (p_old != p_end)
-                {
-                    p_old = p_end;
-                    size++; // 更新hex长度
-                }
-                else
-                {
-                    printf("\r\nhex decode success.");
-                    dump_hex(buff_hex, size, size > 8 ? 16 : 8); // 打印数据块
-                    break;
-                }
-            }
-
-            /* base64编码 */
-            char *buff = malloc(BASE64_ENCODE_OUT_SIZE(size)); // 申请内存
-            if (!buff)
-            {
-                printf("buff Low memory! size = %d\r\n", size);
-                /* 释放内存 */
-                free(buff_hex);
-
-                return -1;
-            }
-            uint16_t len;
-            len = base64_encode((const uint8_t *)buff_hex, size, buff);
-            printf("\r\nbase64 encode buff success.\r\n");
-            printf("in size = %d out length = %d", size, len);
-            size = len;                              // 实际长度
-            dump_hex(buff, size, size > 8 ? 16 : 8); // 打印数据块
-            printf("\r\n");
-
-            /* 释放内存 */
-            free(buff_hex);
-            free(buff);
-
-            return 0;
+            spaceCount++;
         }
-        else if (!strcmp(argv[2], "str"))
+    }
+    spaceCount += 2; // 至少2个数用于申请内存
+    // printf("spaceCount = %d\r\n", spaceCount);
+
+    /* 解码hex字符串 */
+    char *buff_hex = malloc(spaceCount); // 申请内存
+    if (!buff_hex)
+    {
+        printf("buff_hex Low memory! size = %d\r\n", spaceCount);
+
+        return -1;
+    }
+    char *p_end = argv[3];
+    char *p_old = 0;
+    size = 0; // 初始化hex长度
+    for (size_t i = 0; i < spaceCount; i++)
+    {
+        // 使用strtol函数将字符串转换为整数，并存储到buff_hex中
+        buff_hex[i] = strtol(p_end, &p_end, 0);
+
+        if (p_old != p_end)
         {
-            if (argc <= 3)
-            {
-
-                printf("write parameter Error.\r\n");
-                printf("%s ", argv[0]);
-                printf("%s ", argv[1]);
-                printf("%s ", argv[2]);
-                printf("xxx string\r\n");
-
-                return -1;
-            }
-
-            uint16_t size = strlen(argv[3]);
-            /* base64编码 */
-            char *buff = malloc(BASE64_ENCODE_OUT_SIZE(size)); // 申请内存
-            if (!buff)
-            {
-                printf("buff Low memory! size = %d\r\n", size);
-
-                return -1;
-            }
-            uint16_t len;
-            len = base64_encode((const uint8_t *)argv[3], size, buff);
-            printf("\r\nbase64 encode buff success.\r\n");
-            printf("in size = %d out length = %d", size, len);
-            size = len;                              // 实际长度
-            dump_hex(buff, size, size > 8 ? 16 : 8); // 打印数据块
-            printf("\r\n");
-
-            /* 释放内存 */
-            free(buff);
-
-            return 0;
+            p_old = p_end;
+            size++; // 更新hex长度
         }
         else
         {
-            printf("write parameter Error.\r\n");
-            printf("%s ", argv[0]);
-            printf("%s\r\n", help_info[0]);
-            return -1;
+            printf("\r\nhex decode success.");
+            dump_hex(buff_hex, size, size > 8 ? 16 : 8); // 打印数据块
+            break;
         }
     }
-    else if (!strcmp(argv[1], "decode")) // 解码
+
+    /* base64编码 */
+    char *buff = malloc(BASE64_ENCODE_OUT_SIZE(size)); // 申请内存
+    if (!buff)
     {
+        printf("buff Low memory! size = %d\r\n", size);
+        /* 释放内存 */
+        free(buff_hex);
 
-        if (argc <= 2)
-        {
-            printf("write parameter Error.\r\n");
-            printf("%s ", argv[0]);
-            printf("%s\r\n", help_info[1]);
+        return -1;
+    }
+    uint16_t len;
+    len = base64_encode((const uint8_t *)buff_hex, size, buff);
+    printf("\r\nbase64 encode buff success.\r\n");
+    printf("in size = %d out length = %d", size, len);
+    size = len;                              // 实际长度
+    dump_hex(buff, size, size > 8 ? 16 : 8); // 打印数据块
+    printf("\r\n");
 
-            return -1;
-        }
+    /* 释放内存 */
+    free(buff_hex);
+    free(buff);
 
-        uint16_t size;
-        /* base64解码 */
-        size = strlen(argv[2]);
-        char *buff = malloc(BASE64_DECODE_OUT_SIZE(size));
-        if (!buff)
-        {
-            printf("Low memory! size = %d\r\n", size);
-        }
+    return 0;
+}
 
-        uint16_t len;
-        len = base64_decode((const char *)argv[2], size, (uint8_t *)buff);
-        if (!len)
-        {
-            printf("base64 decode failed.\r\n");
+// 输入编码为字符串
+static int encodeString(int argc, char *argv[])
+{
+    if (argc <= 3)
+    {
+        printError("Parameter error.");
+        printUsage(argv[0], "encode str xxx string");
+        return -1;
+    }
 
-            /* 释放内存 */
-            free(buff);
+    uint16_t size = strlen(argv[3]);
+    /* base64编码 */
+    char *buff = malloc(BASE64_ENCODE_OUT_SIZE(size)); // 申请内存
+    if (!buff)
+    {
+        printf("buff Low memory! size = %d\r\n", size);
 
-            return -1;
-        }
+        return -1;
+    }
+    uint16_t len;
+    len = base64_encode((const uint8_t *)argv[3], size, buff);
+    printf("\r\nbase64 encode buff success.\r\n");
+    printf("in size = %d out length = %d", size, len);
+    size = len;                              // 实际长度
+    dump_hex(buff, size, size > 8 ? 16 : 8); // 打印数据块
+    printf("\r\n");
 
-        printf("base64 decode buff success. in size = %d out length = %d\r\n",
-               size, len);
-        size = len;                              // 实际长度
-        dump_hex(buff, size, size > 8 ? 16 : 8); // 打印数据块
+    /* 释放内存 */
+    free(buff);
+
+    return 0;
+}
+
+// 解码
+static int decode(int argc, char *argv[])
+{
+    if (argc <= 2)
+    {
+        printError("Parameter error.");
+        printUsage(argv[0], "decode xxx string");
+        return -1;
+    }
+
+    uint16_t size;
+    /* base64解码 */
+    size = strlen(argv[2]);
+    char *buff = malloc(BASE64_DECODE_OUT_SIZE(size));
+    if (!buff)
+    {
+        printf("Low memory! size = %d\r\n", size);
+    }
+
+    uint16_t len;
+    len = base64_decode((const char *)argv[2], size, (uint8_t *)buff);
+    if (!len)
+    {
+        printf("base64 decode failed.\r\n");
 
         /* 释放内存 */
         free(buff);
 
-        return 0;
+        return -1;
+    }
+
+    printf("base64 decode buff success. in size = %d out length = %d\r\n",
+           size, len);
+    size = len;                              // 实际长度
+    dump_hex(buff, size, size > 8 ? 16 : 8); // 打印数据块
+
+    /* 释放内存 */
+    free(buff);
+
+    return 0;
+}
+
+#if defined(__SHELL_H__) && defined(DEBUG_MODE)
+static int _cmd(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        printf("Error: Invalid command.\r\nUsage:\r\n");
+        printf("%s encode hex/str\r\n", argv[0]);
+        printf("%s decode string\r\n", argv[0]);
+        return -1;
+    }
+
+    if (strcmp(argv[1], "encode") == 0)
+    {
+        if (argc < 3)
+        {
+            printError("Missing encoding type.");
+            printUsage(argv[0], "encode hex/str");
+            return -1;
+        }
+
+        if (strcmp(argv[2], "hex") == 0)
+        {
+            return encodeHex(argc, argv);
+        }
+        else if (strcmp(argv[2], "str") == 0)
+        {
+            return encodeString(argc, argv);
+        }
+        else
+        {
+            printError("Invalid encoding type.");
+            printUsage(argv[0], "encode hex/str");
+            return -1;
+        }
+    }
+    else if (strcmp(argv[1], "decode") == 0)
+    {
+        return decode(argc, argv);
     }
     else
     {
-        printf("Error: Invalid command.\r\nUsage:\r\n");
-        for (uint32_t i = 0; i < sizeof(help_info) / sizeof(char *); i++)
-        {
-            printf("%s ", argv[0]);
-            printf("%s\r\n", help_info[i]);
-        }
-        printf("\r\n");
+        printError("Invalid command.");
+        printUsage(argv[0], "encode/decode");
+
+        return -1;
     }
 
-    return -1;
+    return 0;
 }
+
 // 导出到命令列表里
 SHELL_EXPORT_CMD(SHELL_CMD_PERMISSION(0) | SHELL_CMD_TYPE(SHELL_TYPE_CMD_MAIN), base64, _cmd, base64[encode decode]);
 #endif // #ifdef DEBUG_MODE
